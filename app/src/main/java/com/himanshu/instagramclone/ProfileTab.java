17b2450ee450ccb2b73453772ca400376db3e469
himanshu <bhimanshu031@gmail.com>
2020-06-02 03:10:05 +0530

    Implemented the Profile Tab and User Tab.

--- app/src/main/java/com/himanshu/instagramclone/ProfileTab.java
+++ app/src/main/java/com/himanshu/instagramclone/ProfileTab.java
@@ -7,14 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileTab extends Fragment {
+public class ProfileTab extends Fragment
+{
+    // UI COMPONENTS USED BY THE PROFILE TAB FRAGMENT
+    private EditText edtProfileName , edtProfileBio , edtProfileProfession , edtProfieHobbies , edtProfileFavSport;
+    private Button btnUpdateInfo;
 
-    public ProfileTab() {
+    public ProfileTab()
+    {
         // Required empty public constructor
     }
 
@@ -23,6 +36,97 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        // return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        // now initialise the components of the fragment
+
+        edtProfileName = view.findViewById(R.id.edtProfileName);        // as here view will contain the reference to all the UI components present in fragment.
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfieHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfileFavSport = view.findViewById(R.id.edtProfileFavSport);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();       // gives the current parse user
+
+        // IF THE USER ALREADY UPDATED THE VALUES CORRESPONDING TO THE EDIT TEXT THEN IT WILL SHOW THOSE UPDATED VALUES OTHERWISE EMPTY STRING
+        if(parseUser.get("profileName") == null)        // IF VALUE RETURNED FROM SERVER IS NULL CORRESPONDING TO THE KEY
+        {
+            edtProfileName.setText("");
+        }
+        else
+        {
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+
+        if(parseUser.get("profileBio") == null)
+        {
+            edtProfileBio.setText("");
+        }
+        else
+        {
+            edtProfileBio.setText(parseUser.get("profileBio") + "");
+        }
+
+        if(parseUser.get("profileProfession") == null)
+        {
+            edtProfileProfession.setText("");
+        }
+        else
+        {
+            edtProfileProfession.setText(parseUser.get("profileProfession") + "");
+        }
+
+        if(parseUser.get("profileHobbies") == null)
+        {
+            edtProfieHobbies.setText("");
+        }
+        else
+        {
+            edtProfieHobbies.setText(parseUser.get("profileHobbies") + "");
+        }
+
+        if(parseUser.get("profileFavSport") == null)
+        {
+            edtProfileFavSport.setText("");
+        }
+        else
+        {
+            edtProfileFavSport.setText(parseUser.get("profileFavSport") + "");
+        }
+
+
+        // setting onClickListener
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                parseUser.put("profileName" , edtProfileName.getText().toString());       // value of Profile name updated on dashbboard for current parse user.
+                parseUser.put("profileBio" , edtProfileBio.getText().toString());
+                parseUser.put("profileProfession" , edtProfileProfession.getText().toString());
+                parseUser.put("profileHobbies" , edtProfieHobbies.getText().toString());
+                parseUser.put("profileFavSport" ,edtProfileFavSport.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e)
+                    {
+                        if(e == null)
+                        {
+                            FancyToast.makeText(getContext() , "Info is updated" , Toast.LENGTH_SHORT , FancyToast.INFO , true).show();
+                        }
+                        else
+                        {
+                            FancyToast.makeText(getContext() , e.getMessage() , Toast.LENGTH_SHORT , FancyToast.ERROR , true).show(); // error message
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/himanshu/instagramclone/UserTab.java
+++ app/src/main/java/com/himanshu/instagramclone/UserTab.java
@@ -7,6 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,15 +25,62 @@ import android.view.ViewGroup;
  */
 public class UserTab extends Fragment {
 
-    public UserTab() {
+    private ListView listView;
+    private ArrayList arrayList;
+
+    private ArrayAdapter arrayAdapter;          // used as controller b/w listView and arrayList
+    public UserTab()
+    {
         // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_tab, container, false);
+        View view =  inflater.inflate(R.layout.fragment_user_tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+
+        arrayAdapter = new ArrayAdapter(getContext() , android.R.layout.simple_list_item_1 , arrayList);        // as we want arrayAdapter to adapt the arrayList
+
+        final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
+
+
+        final ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username" , ParseUser.getCurrentUser().getUsername());      // as we don't query to contain the current user as
+                                                                                               // it is already interacting with the app.
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>()           // to get data from server and save it to arrayList
+        {
+            @Override
+            public void done(List<ParseUser> users, ParseException e)
+            {
+                if(e == null)
+                {
+                    if(users.size() > 0)
+                    {
+                        for(ParseUser user : users)
+                        {
+                            arrayList.add(user.getUsername());      // saving username of the object to the arrayList
+                        }
+
+                        listView.setAdapter(arrayAdapter);          // setting the adapter for the listView which is arrayAdapter which adapts ArrayList
+
+                        // ONCE THE LISTVIEW HAS GOT THE DATA FROM THE SERVER WE WANT TEXTVIEW TO VANISH AND LISTVIEW TO BECOME VISIBLE.
+                        txtLoadingUsers.animate().alpha(0).setDuration(2000);
+
+                        listView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+
+        return view;
     }
 }
